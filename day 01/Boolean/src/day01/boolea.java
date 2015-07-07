72cb70d3a827025926959d4b4d6b841180a45e34
xiaosuo <1219770950@qq.com>
2015-07-07 20:05:54 +0800

    代码上传（第一次）
    我是李映鲲耶，代码托管开启

---
+++ day 01/Boolean/src/day01/boolea.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class boolea {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag1=true;
+        boolean flag2=false;
+        System.out.println("flag1 "+flag1);
+        System.out.println("flag2 "+flag2);
+	}
+
+}
---
+++ day 01/Cha/src/day01/cha.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class cha {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		       char ch1='a';
+		       char ch2='¿Ó';
+		       System.out.println(ch1);
+		       System.out.println(ch2);
+			}
+
+		
+	}
+
+
